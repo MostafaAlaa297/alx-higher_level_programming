756a43ac5892ed6b3ad7ea6a4f0dc975bf0b7ca8
MostafaAlaa297 <elaskary609@gmail.com>
2023-11-28 14:44:18 -0800

    Create uppercase prog

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uppercase(c):
     for char in c:
-        print("{:c}".format(ord(char) - 32) if ord(char) >= 97 and ord(char) <= 122 else char, end="")
+        print("{:c}".format(ord(char)-32) if ord(char)>=97 and ord(char)<=122 else char, end="")
     print()

a2dd4c47c529095096ebe2608d22ae377efe2d35
MostafaAlaa297 <elaskary609@gmail.com>
2023-11-30 10:43:27 -0800

    Edit print shell args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+def print_args(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("0 arguments")
+    else:
+        if n == 1:
+            print("1 argument:")
+        else:
+            print("{:d} arguments:".format(n))
+        for i, arg in enumerate(argv[1:], start=1):
+            print("{:d}: {:s}".format(i, arg))
+
 if __name__ == "__main__":
-    from sys import argv
-
-    length = len(argv) - 1
-    plural = "s" if length != 1 else ""
-    period = "." if length == 0 else ":"
-
-    print("{:d} argument{:s}{:s}".format(length, plural, period))
-
-    for i, arg in enumerate(argv[1:]):
-        print("{:d}: {:s}".format(i, arg))
+    import sys
+    print_args(sys.argv)

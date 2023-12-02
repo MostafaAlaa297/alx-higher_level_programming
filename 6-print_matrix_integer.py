7498265de88cdf6ec337d33f24154e3ffb5e5176
MostafaAlaa297 <elaskary609@gmail.com>
2023-12-02 06:36:42 -0800

    Edit print matrix

--- 6-print_matrix_integer.py
+++ 6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
     else:
         for row in matrix:
             for column in row:
-                print('{:d}'.format(column), end=' '
-                      if column != row[-1] else '\n')
+                print('{:d}'.format(column), end=" ")
+            print()

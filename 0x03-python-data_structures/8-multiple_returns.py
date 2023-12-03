735722cf76337093ffb35a36484f6938cfe937bb
MostafaAlaa297 <elaskary609@gmail.com>
2023-12-03 06:57:55 -0800

    multiply returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        sentence[0] = None
+    if not sentence:
+        return (0, None)
     else:
-        a = tuple(sentence)
-        first = a[0]
-        length = len(a)
-        return length, first
-
+        return (len(sentence), sentence[0])

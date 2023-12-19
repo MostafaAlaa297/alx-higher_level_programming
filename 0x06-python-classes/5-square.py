995733c6e90fbdd3bc84cea6e901c98f6122dc13
MostafaAlaa297 <elaskary609@gmail.com>
2023-12-19 11:28:14 -0800

    print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""5-square.py"""
+class Square:
+    """
+    Defines a square.
+
+    Attributes:
+        __size (int): The size of the square
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new square instance
+
+        Args:
+        size (int, optional): The size of the square. Defaults to 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        if value < 0:
+            raise ValueError
+
+        self.__size = value
+
+    @property
+    def area(self):
+        """Compute the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            limit = self.__size
+            i = 0
+            while i < limit:
+                for j in range (limit):
+                    print("#", end="")
+                    j += 1
+                print()
+                i += 1

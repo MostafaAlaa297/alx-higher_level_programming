074efd50674a2bcb8f92e877c876a0754c837608
MostafaAlaa297 <elaskary609@gmail.com>
2023-12-19 08:32:03 -0800

    Add size attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square Module."""
+class Square:
+    """
+    Defines a square.
+
+    Attributes:
+        size (int): The size of the square
+    """
+    def __init__(self, size):
+        self.__size = size

b8323f7e9e0d132f5a82aa2bec251b36edd23ed2
MostafaAlaa297 <elaskary609@gmail.com>
2023-12-19 08:05:20 -0800

    doc for square class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,54 +1,6 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Square Class Documentation:
-
-This module defines a simple Square class in Python.
-
-Example:
-    Examples can be given using either the ``Example`` or ``Examples``
-    sections. Sections support any reStructuredText formatting, including
-    literal blocks::
-
-        $ python example_square.py
-
-Attributes:
-    module_level_variable1 (int): Module level variables may be documented in
-        either the ``Attributes`` section of the module docstring or in an
-        inline docstring immediately following the variable.
-
-        Either form is acceptable, but the two should not be mixed. Choose
-        one convention to document module-level variables and be consistent
-        with it.
-
-Todo:
-    * For module TODOs
-    * You have to also use ``sphinx.ext.todo`` extension
-
-.. _Google Python Style Guide:
-   http://google.github.io/styleguide/pyguide.html
-
-"""
-
-module_level_variable1 = 12345
-
-module_level_variable2 = 98765
-"""int: Module level variable documented inline.
-
-The docstring may span multiple lines. The type may optionally be specified
-on the first line, separated by a colon.
-"""
-
 class Square:
     """
-    Square Class:
-
-    A basic class representing a square.
-
-    Attributes:
-        None
-
-    Methods:
-        None
+    An empty class that represents a square
     """
     pass

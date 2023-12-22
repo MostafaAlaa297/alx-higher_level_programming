a40af72f37f6bb8845d4eca118b0dc15f5339d7f
MostafaAlaa297 <elaskary609@gmail.com>
2023-12-22 10:50:02 -0800

    Give index of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,10 +10,10 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index = hash_djb2(key);
+	unsigned long int index = hash_djb2(key) % size;
 
 	if (index > size)
 		return (-1);
 	
-	return (index);
+	return (index % size);
 }

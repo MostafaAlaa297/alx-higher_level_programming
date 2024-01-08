e473a94d4f99857c7c4fc53475ab39c0d7327df8
MostafaAlaa297 <elaskary609@gmail.com>
2024-01-08 10:48:53 -0800

    base_geometry with added base_geometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python3
-"""
-==================
-module BaseGeometry
-==================
-"""
-
 class BaseGeometry:
-    """class with public instance method"""
+    """
+    BaseGeometry class.
+    """
+
     def area(self):
-        """Public instance method that raises an Exception"""
+        """
+        Raises an Exception with the message 'area() is not implemented'.
+        """
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """
         Validates the value and raises appropriate exceptions.

0e307497156656ad2f2d87fd7e6bbab4700acce1
MostafaAlaa297 <elaskary609@gmail.com>
2024-01-09 10:31:22 -0800

    convert from json to data obj

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+===============
+file I/O module
+===============
+"""
+import json
+def from_json_string(my_str):
+    """
+    return from JSON
+    """
+    return json.loads(my_str)

d45b6221cfba5648311b93b047f75ec46b168355
MostafaAlaa297 <elaskary609@gmail.com>
2024-01-09 09:24:13 -0800

    readfile

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+=================
+file I/O module
+=================
+"""
+
+def read_file(filename=""):
+    """
+    reading a file in utf-8
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        file.read()

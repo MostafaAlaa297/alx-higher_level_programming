a92f01367e330a5c19b3c03e5e504f153ad8994d
MostafaAlaa297 <elaskary609@gmail.com>
2024-01-13 06:55:41 -0800

    add update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,5 +79,18 @@ class Rectangle(Base):
             result = " " * self.__x + "#" * self.__width
             print(result)
 
+    def update(self, *args):
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
+
+
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)

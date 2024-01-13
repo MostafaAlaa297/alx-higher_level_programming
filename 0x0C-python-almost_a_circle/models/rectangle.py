2ed848fab8524e5a94ddbd6747fdd98ce05f2acd
MostafaAlaa297 <elaskary609@gmail.com>
2024-01-13 08:41:53 -0800

    add kwargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,17 +79,21 @@ class Rectangle(Base):
             result = " " * self.__x + "#" * self.__width
             print(result)
 
-    def update(self, *args):
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.__width = args[1]
-        if len(args) > 2:
-            self.__height = args[2]
-        if len(args) > 3:
-            self.__x = args[3]
-        if len(args) > 4:
-            self.__y = args[4]
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
 
     def __str__(self):

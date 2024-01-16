d224bcc358c9c452b202de7e91f9e240a53e6741
MostafaAlaa297 <elaskary609@gmail.com>
2024-01-15 18:58:16 -0800

    add changes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,7 @@ class Square(Rectangle):
     """
     Square class inherits from Rectangle
     """
+     __nb_objects = 0
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 

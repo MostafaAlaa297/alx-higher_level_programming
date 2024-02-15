a3d0f39f658b8ac65aa952029f17bff877be3a5e
MostafaAlaa297 <elaskary609@gmail.com>
2024-02-14 16:04:03 -0800

    join states and cities

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,4 @@
 -- Lists cities contained in the database
-SELECT cities.id, cities.name, states.name
-FROM cities
-JOIN states
-ON cities.id = states.id
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;

66fa0584223d93ca8f9f86f2f7c39371592b5f0f
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-13 00:40:26 +0000

    tasks after semistandard

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-	return function (value) {	
-		return value.toString(base);
-	}
-}
+  return function (value) {
+    return value.toString(base);
+  };
+};
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,20 +3,17 @@
 const ParentSquare = require('./5-square.js');
 
 class Square extends ParentSquare {
-	constructor (size) {
-		super(size);
-	}
-	charPrint(c) {
-		if (c === undefined) {
-			for (let i = 0; i < this.size; i++) {
-				console.log('X'.repeat(this.size));
-			}
-		} else {
-                	for (let i = 0; i < this.size; i++) {
-				console.log(c.repeat(this.size));
-			}
-		}
-	}
+  charPrint (c) {
+    if (c === undefined) {
+      for (let i = 0; i < this.size; i++) {
+        console.log('X'.repeat(this.size));
+      }
+    } else {
+      for (let i = 0; i < this.size; i++) {
+        console.log(c.repeat(this.size));
+      }
+    }
+  }
 }
 
 module.exports = Square;

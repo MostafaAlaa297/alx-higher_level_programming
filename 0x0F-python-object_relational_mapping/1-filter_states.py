4761997e24b23c0ba5c4884e2cdde5a1ce9b6040
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-16 14:06:38 -0700

    filter n

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+Get state ids and names
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    Main funtion
+    """
+    conn = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            host="localhost",
+            port=3306
+            )
+
+    cur = conn.cursor()
+
+    cur.execute("""
+            SELECT id, name
+            FROM states
+            WHERE name LIKE 'N%'
+            ORDER BY id ASC;
+            """)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()

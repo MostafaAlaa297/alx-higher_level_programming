ebb801ac7612a295b3f13bbdbbfefc370326295e
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-17 15:13:34 -0700

    model state my get

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+Get state ids and names
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def main():
+    """
+    Main funtion
+    """
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+            )
+    Base.metadata.create_all(engine)
+
+    my_session = sessionmaker(bind=engine)
+
+    session = my_session()
+
+    row = session.query(State).filter(State.name==sys.argv[4]).first()
+
+    if (row):
+        print("{}".format(row.id))
+    else:
+        print("Not found")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()

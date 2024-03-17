032c800491ec7ec07012506a87d597944c590b24
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-17 08:27:31 -0700

    tasks

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -19,13 +19,13 @@ def main():
             .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
             )
     Base.metadata.create_all(engine)
-    
+
     my_session = sessionmaker(bind=engine)
 
     session = my_session()
 
     rows = session.query(State).order_by(State.id).all()
-    
+
     for i, row in enumerate(rows, 1):
         print("{}: {}".format(i, row.name))
 

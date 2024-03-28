f06749b31ed65b8b8650cff660d771b696820974
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-27 17:30:15 -0700

    methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept
-curl -s -i -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f2
+curl -s -I -X OPTIONS "$1" | grep 'Allow:' | cut -d' ' -f2

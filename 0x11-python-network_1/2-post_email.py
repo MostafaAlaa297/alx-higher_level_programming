16a93a8acca4836dced2797b31df3f574e60156f
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-28 16:53:22 -0700

    modify post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,10 +8,10 @@ Status Module
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = {'email': email}
-    data = urllib.parse.urlencode(data).encode('utf-8')
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
+    payload = {'email': email}
+    payload = urllib.parse.urlencode(payload)
+    payload = payload.encode('ascii')
+    req = urllib.request.Request(url, payload)
 
     with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')

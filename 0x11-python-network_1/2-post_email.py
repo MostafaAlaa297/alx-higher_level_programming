26cdc02abb2df76eac407d7fe08f7509f0ebda72
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-29 06:44:13 -0700

    fix 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,13 +6,19 @@ import sys
 Status Module
 """
 if __name__ == "__main__":
+    import urllib.parse
+    import urllib.request
+    import sys
+
     url = sys.argv[1]
     email = sys.argv[2]
+    
     payload = {'email': email}
     payload = urllib.parse.urlencode(payload)
     payload = payload.encode('ascii')
-    req = urllib.request.Request(url, payload)
+    #req = iurllib.request.Request(url, payload)
 
-    with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen(url, payload) as response:
+        print("Your email is:", email)
         body = response.read().decode('utf-8')
         print(body)

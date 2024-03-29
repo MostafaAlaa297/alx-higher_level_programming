662fa62017765cec5989da458d515933230d685b
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-28 18:04:06 -0700

    post email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Status Module
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data= {'email': email}
+
+    response = requests.post(url, data=data) 
+    print("Your email is:", response.text)

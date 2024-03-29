e3a109b7ae57ff895ee2a8585a4a042c483123eb
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-29 06:27:18 -0700

    connect to github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,8 +9,10 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
 
-    res = requests.get("https://api.github.com/user", auth = (username, password))
-    
+    res = requests.get(
+            "https://api.github.com/user",
+            auth=(username, password)
+            )
     if res.status_code == 200:
         data = res.json()
         print(data.get('id'))

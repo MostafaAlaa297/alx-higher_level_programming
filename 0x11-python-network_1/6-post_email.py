6f9d152c98ba5768df5601116789cc176eb4d34e
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-28 18:07:42 -0700

    pycodestyle

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data= {'email': email}
+    data = {'email': email}
 
-    response = requests.post(url, data=data) 
+    response = requests.post(url, data=data)
     print(response.text)

5d10e0b9cddd325c7359e24c98abf5ffd289e8a8
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-28 17:31:57 -0700

    status with requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-import requests
 """
 Status Module
 """
 if __name__ == "__main__":
-    res = requests.get('https://alx-intranet.hbtn.io/status')
+    import requests
+    url = 'https://alx-intranet.hbtn.io/status'
+    res = requests.get(url)
+
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    print("\t- type:", type(res.text))
+    print("\t- content:", res.text)

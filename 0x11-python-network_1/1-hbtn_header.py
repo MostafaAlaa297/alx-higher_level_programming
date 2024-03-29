a94fded86f1dbb592fc1653e0b2f8afc0022fc58
MostafaAlaa297 <elaskary609@gmail.com>
2024-03-29 06:36:54 -0700

    fix 1-hbtn_header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,7 @@ Status Module
 if __name__ == "__main__":
     url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    headers = response.headers
-    x_req_id = headers.get("X-Request-Id")
-    print(x_req_id)
+    with urllib.request.urlopen(url) as response:
+        headers = response.headers
+        x_req_id = headers.get("X-Request-Id")
+        print(x_req_id)

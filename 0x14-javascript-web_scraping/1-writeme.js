0c3f2c1da92b73ae0ca45b799dd5892e08dc8414
MostafaAlaa297 <elaskary609@gmail.com>
2024-04-23 15:53:31 -0700

    write

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require("fs");
+
+if (process.argv.length < 3){
+	console.error("Usage: node 0-readme.js <file_path>");
+	process.exit(1);
+}
+
+const file = process.argv[2];
+const input = process.argv[3];
+
+fs.writeFile(file, input, (err) => {
+	if (err){
+		console.error(err);
+		process.exit(1);
+	}
+});

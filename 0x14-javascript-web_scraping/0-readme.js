c205f5a9624407d0c75dac8f4ff630ec6a28271e
MostafaAlaa297 <elaskary609@gmail.com>
2024-04-23 15:56:46 -0700

    fix style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 
-if (process.argv.length != 3){
-	console.error("Usage: node 0-readme.js <file_path>");
-	process.exit(1);
+if (process.argv.length !== 3) {
+  console.error('Usage: node 0-readme.js <file_path>');
+  process.exit(1);
 }
 
 const file = process.argv[2];
 
-fs.readFile(file, "utf-8", (err, data) => {
-	if (err){
-		console.error(err);
-		process.exit(1);
-	}
-	console.log(data);
+fs.readFile(file, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(data);
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 
-if (process.argv.length < 3){
-	console.error("Usage: node 0-readme.js <file_path>");
-	process.exit(1);
+if (process.argv.length < 3) {
+  console.error('Usage: node 0-readme.js <file_path>');
+  process.exit(1);
 }
 
 const file = process.argv[2];
 const input = process.argv[3];
 
 fs.writeFile(file, input, (err) => {
-	if (err){
-		console.error(err);
-		process.exit(1);
-	}
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
 });

c9039c667c3b0c933391a7e3a2a97b5b7f211086
MostafaAlaa297 <elaskary609@gmail.com>
2024-04-23 17:12:26 -0700

    ID 18

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  } else if (response.statusCode !== 200) {
+    console.error(`Received code status: ${response.statusCode}`);
+    process.exit(1);
+  } else {
+    const films = JSON.parse(body);
+    let count = 0;
+
+    for (const film of films.results) {
+	if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+		count++;
+	}
+    }
+
+    // Print the title of the movie
+    console.log(count);
+  }
+});
